b65f7280fc056903bff1b35414864467e39cc386
ignaciocuyun <ignacio.cuyun@telus.com>
2023-04-24 23:16:30 -0600

    finished

--- js/main.js
+++ js/main.js
@@ -1,37 +1,21 @@
+
+
+
+
 $(document).ready(function() {
-  // Mostrar las imágenes cuando se haga clic en el botón "Mostrar imágenes"
+  $("#mostrar-imagenes").hide();
+  
   $("#mostrar-imagenes").click(function() {
     $("img").show();
+    $(this).hide();
+    $("#ocultar-imagenes").show();
+    $(".fab-container").addClass("active");
   });
-
-  // Ocultar las imágenes cuando se haga clic en el botón "Ocultar imágenes"
+  
   $("#ocultar-imagenes").click(function() {
+    $(this).hide();
     $("img").hide();
+    $("#mostrar-imagenes").show();
+    $(".fab-container").removeClass("active");
   });
 });
-
-
-// Obtener todas las secciones de la página
-const secciones = document.querySelectorAll('.seccion');
-
-// Función para detectar si una sección está visible en la pantalla
-function estaVisible(elemento) {
-  const rect = elemento.getBoundingClientRect();
-  const altura = window.innerHeight || document.documentElement.clientHeight;
-  return rect.top <= altura && rect.bottom >= 0;
-}
-
-// Función para agregar una clase de animación cuando una sección está visible
-function animarSeccion() {
-  for (let i = 0; i < secciones.length; i++) {
-    if (estaVisible(secciones[i])) {
-      secciones[i].classList.add('animar');
-    }
-  }
-}
-
-// Agregar un listener para detectar cuando el usuario hace scroll por la página
-window.addEventListener('scroll', animarSeccion);
-
-// Ejecutar la función una vez para animar las secciones que están visibles al cargar la página
-animarSeccion();
